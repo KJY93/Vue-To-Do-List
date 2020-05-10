88768b9f977587222c01dc09407b0d546c494975
KJY93 <38803618+KJY93@users.noreply.github.com>
2020-05-10 17:30:53 +0800

    added entrypoint.sh file to check database health state before applying migrations and run the Django dev server

---
+++ backend/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# entrypoint file to verify Postgres is healthy before applying
+# migrations and running the Django dev server
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
